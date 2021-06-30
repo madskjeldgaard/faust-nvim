8b8f9fe93569ec20c35f2adf370f56d96bbd01ab
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-06-30 12:21:03 +0200

    help file generator

---
+++ scripts/generate_helpfiles.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+# function sed-get-between-duplicate() {
+#     sed -n '/'$1'/,/'$1'/{/'$1'/b;/'$1'/b;p}' $2
+# }
+#
+# for FAUSTFILE in "$(find . /usr/share/faust | grep "\.lib" )"; do
+DATADIR=data
+HELPDIR=doc
+FAUSTLIB=/usr/share/faust
+
+function detect_architecture(){
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		# LINUX
+		if [[ $(uname -m) == "x86_64" ]]; then
+			ARCHICTECTURE="linux_amd64"
+		else
+			ARCHICTECTURE="linux_arm"
+		fi
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		# MACOS
+		ARCHICTECTURE="darwin_amd64" 
+	elif [[ "$OSTYPE" == "cygwin" ]]; then
+		ARCHICTECTURE="windows_amd64" 
+	else
+		echo "$OSTYPE unknown" && exit 1
+	fi
+
+}
+
+function get_converter(){
+	CFILE="md2vim_$ARCHICTECTURE.tar.gz"
+	URL="https://foosoft.net/projects/md2vim/dl/$CFILE"
+	CONVERTER=$DATADIR/${CFILE%.tar.gz}/md2vim
+
+	wget $URL --directory-prefix=$DATADIR/ &&\
+		tar -xvf $DATADIR/$CFILE --directory=$DATADIR/ && \
+		rm $DATADIR/$CFILE
+}
+
+function init(){
+	mkdir -p $DATADIR
+
+	detect_architecture && \
+	get_converter && \
+		faustlib2markdownfiles
+}
+
+function faustlib2markdownfiles(){
+	FFILES=$(ls -a1 $FAUSTLIB | grep "\.lib")
+
+	mkdir -p $HELPDIR
+	mkdir -p $DATADIR/md
+
+	for FAUSTFILE in $FFILES; do
+		FAUSTMD="$DATADIR/md/${FAUSTFILE%.lib}.md"
+		FAUSTHELPFILE="$HELPDIR/${FAUSTFILE%.lib}.txt"
+
+		# Convert faust lib to markdown
+		faust2md "$FAUSTLIB/$FAUSTFILE"	> $FAUSTMD 
+
+		# Call converter and create
+		./$CONVERTER $FAUSTMD $FAUSTHELPFILE
+
+	done
+
+}
+
+init
