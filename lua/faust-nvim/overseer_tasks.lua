8ac96afac94935fe7587910e5d7737451a430d7d
mads <mail@madskjeldgaard.dk>
2022-08-09 16:43:37 +0200

    initial overseer support

---
+++ lua/faust-nvim/overseer_tasks.lua
@@ -0,0 +1,47 @@
+local status, overseer = pcall(require, 'overseer')
+
+if status then
+	-- Build
+	overseer.register_template({
+		name = "faust2sc",
+		builder = function(params)
+			return {
+				cmd = {'faust2sc.py'},
+				args = {},
+				name = "faust2sc",
+				cwd = "",
+				env = {},
+				components = {
+					"default",
+					{"on_output_parse", parser = {
+						-- Put the parser results into the 'diagnostics' field on the task result
+						diagnostics = {
+							-- Extract fields using lua patterns
+							{
+								"extract",
+								"^([^%s].+) : (%d+) : (.+)$",
+								"filename",
+								"lnum",
+								"text"
+							},
+						}
+					}},
+					{"on_result_diagnostics",
+						remove_on_restart = true,
+					},
+					{"on_result_diagnostics_quickfix",
+						open = true
+					}
+				},
+				metadata = {},
+			}
+		end,
+		desc = "Compile Faust file as SuperCollider plugin",
+		tags = {overseer.TAG.BUILD},
+		params = { },
+		priority = 50,
+		condition = {
+			filetype = {"dsp", "faust"},
+		},
+	})
+end
